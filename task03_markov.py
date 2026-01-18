0819d4ec6d07b7752edc5b444157d356c38c7428
vaishnaviemmadi23 <vaishnaviemmadi23@gmail.com>
2026-01-18 18:33:00 +0530

    Update task03_markov.py

--- task03_markov.py
+++ task03_markov.py
@@ -1,9 +1,10 @@
 import random
 from collections import defaultdict
-text ="""
-Generative AI is a powerful technology.
-AI can generate text and images.
-Morkov Chains generate text using probability.
+text = """
+Generative AI is a powerful technology that can create text, images, and code.
+Markov Chains generate new text based on probability.
+This program learns word patterns from the input text and produces new sentences.
+Learning GitHub and Python step by step will help me improve my skills.
 """
 words = text.split()
 model = defaultdict(list)
@@ -14,12 +15,12 @@ for i in range(len(words)-1):
 start_word = random.choice(words)
 result = [start_word]
 
-for _in range(30):
+for _ in range(30):
  current_word= result[-1]
-   if current_word not in model:
+ if current_word not in model:
       break
 
 result.append(random.choice(model[current_word])
              )
 print("Generated Text:")
-print("."join(result))
+print(" ".join(result))
